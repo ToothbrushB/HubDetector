49a2495f1b66a5ee73a88dd5bf391398aa5e65e9
ToothbrushB <49379586+ToothbrushB@users.noreply.github.com>
2026-02-24 15:42:42 -0600

    first commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,171 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.studica.frc.AHRS;
+import com.studica.frc.AHRS.NavXComType;
+
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.math.filter.LinearFilter;
+import edu.wpi.first.math.filter.Debouncer.DebounceType;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.DigitalOutput;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * The methods in this class are called automatically corresponding to each
+ * mode, as described in
+ * the TimedRobot documentation. If you change the name of this class or the
+ * package after creating
+ * this project, you must also update the Main.java file in the project.
+ */
+public class Robot extends TimedRobot {
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  private DigitalOutput dout;
+  private Debouncer debouncer = new Debouncer(0.05, DebounceType.kFalling);
+  private LinearFilter filter = LinearFilter.singlePoleIIR(0.1, 0.005);
+  private AHRS ahrs;
+  private AddressableLED leds;
+  private AddressableLEDBuffer ledBuffer = new AddressableLEDBuffer(500);
+
+  
+
+  /**
+   * This function is run when the robot is first started up and should be used
+   * for any
+   * initialization code.
+   */
+  public Robot() {
+    super(0.005);
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+    ahrs = new AHRS(NavXComType.kMXP_SPI);
+    ahrs.reset();
+    dout = new DigitalOutput(0);
+    leds = new AddressableLED(9);
+    leds.setLength(ledBuffer.getLength());
+    leds.start();
+
+  }
+
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items
+   * like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+    Translation3d accel = new Translation3d(ahrs.getRawAccelX(), ahrs.getRawAccelY(), ahrs.getRawAccelZ());
+    double accelNorm = Math.abs(accel.getNorm() - 1);
+    double accelFiltered = filter.calculate(accelNorm);
+    SmartDashboard.putNumber("AccelMagnitude", accelNorm);
+    SmartDashboard.putNumber("AccelFiltered", accelFiltered);
+    if (debouncer.calculate(accelFiltered > .2)) {
+      SmartDashboard.putBoolean("Dout", true);
+      dout.set(true);
+      for (int i = 0; i < ledBuffer.getLength(); i++)
+        ledBuffer.setRGB(i, 0, 255, 0);
+    } else {
+      SmartDashboard.putBoolean("Dout", false);
+      dout.set(false);
+      for (int i = 0; i < ledBuffer.getLength(); i++)
+        ledBuffer.setRGB(i, 255, 0, 0);
+    }
+    leds.setData(ledBuffer);
+
+  }
+
+  /**
+   * This autonomous (along with the chooser code above) shows how to select
+   * between different
+   * autonomous modes using the dashboard. The sendable chooser code works with
+   * the Java
+   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the
+   * chooser code and
+   * uncomment the getString line to get the auto name from the text box below the
+   * Gyro
+   *
+   * <p>
+   * You can add additional auto modes by adding additional comparisons to the
+   * switch structure
+   * below with additional strings. If using the SendableChooser make sure to add
+   * them to the
+   * chooser code above as well.
+   */
+  @Override
+  public void autonomousInit() {
+    m_autoSelected = m_chooser.getSelected();
+    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        // Put custom auto code here
+        break;
+      case kDefaultAuto:
+      default:
+        // Put default auto code here
+        break;
+    }
+  }
+
+  /** This function is called once when teleop is enabled. */
+  @Override
+  public void teleopInit() {
+  }
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {
+  }
+
+  /** This function is called once when the robot is disabled. */
+  @Override
+  public void disabledInit() {
+  }
+
+  /** This function is called periodically when disabled. */
+  @Override
+  public void disabledPeriodic() {
+  }
+
+  /** This function is called once when test mode is enabled. */
+  @Override
+  public void testInit() {
+  }
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {
+  }
+
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {
+  }
+
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {
+  }
+}
